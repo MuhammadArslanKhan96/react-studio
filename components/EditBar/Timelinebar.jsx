15448da3cc11c8aa72611dc61ce6a5a71d7d4b1e
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2023-12-15 20:30:03 +0500

    feat: add dynamic data from context

--- components/EditBar/Timelinebar.jsx
+++ components/EditBar/Timelinebar.jsx
@@ -1,51 +1,18 @@
 import React from "react";
-import { Timeline, TimelineEffect, TimelineRow } from "@xzdarcy/react-timeline-editor";
+import { Timeline } from "@xzdarcy/react-timeline-editor";
+import { useAppContext } from "./EditorContext";
 
 export default function Timelinebar() {
-    const mockData = [
-        {
-            id: "0",
-            actions: [
-                {
-                    id: "action00",
-                    start: 0,
-                    end: 2,
-                    effectId: "effect0",
-                },
-            ],
-        },
-        {
-            id: "1",
-            actions: [
-                {
-                    id: "action10",
-                    start: 1.5,
-                    end: 5,
-                    effectId: "effect1",
-                },
-            ],
-        },
-    ];
-
-    const mockEffect = {
-        effect0: {
-            id: "effect0",
-            name: "test",
-        },
-        effect1: {
-            id: "effect1",
-            name: "test1",
-        },
-    };
+    const { mockData, mockEffect, setMockData } = useAppContext();
     return (
         <div>
             <div className="[&>div>div>div>div>div>div>div]:flex [&>div>div>div>div>div>div>div]:items-center [&>div>div>div>div>div>div>div]:justify-around">
                 <Timeline
-                    getActionRender={(action) => (
-                        <div className="flex justify-center items-center text-center">{action.id}</div>
+                    getActionRender={({ id }) => (
+                        <div className="flex justify-center items-center text-center">{id}</div>
                     )}
                     editorData={mockData}
-                    onChange={(a) => console.log(a)}
+                    onChange={(a) => setMockData(a)}
                     effects={mockEffect}
                     style={{ width: "100%", maxHeight: "200px" }}
                 />
